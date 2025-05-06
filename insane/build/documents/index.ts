8690284fe6bfd7ad044017fead1eccf9efc0959d
Romeo Van Snick <romeo@romeovansnick.be>
2025-05-06 19:19:03 +0200

    Hash sources

--- insane/build/documents/index.ts
+++ insane/build/documents/index.ts
@@ -1,22 +1,32 @@
 import { CodeFileLoader } from "@graphql-tools/code-file-loader"
 import { loadDocuments } from "@graphql-tools/load"
-import type { DocumentNode } from "graphql"
-import { concatMap } from "rxjs"
+import { concatMap, distinct } from "rxjs"
 
 import { watch as watchFiles } from "~/build/watch"
+import { hash } from "~/lib/hash"
 
 export type LoadDocumentsOptions = {
 	include?: string[]
 	exclude?: string[]
 }
 
-type Source = {
-	document?: DocumentNode
-	rawSDL?: string
-	location?: string
+export type Source = {
+	hash: string
+	sdl: string
+	location: string
 }
 
-export async function load(options: LoadDocumentsOptions): Promise<Source[]> {
+type FullSource = {
+	rawSDL: string
+	location: string
+}
+
+export type Sources = {
+	hash: string
+	sources: Source[]
+}
+
+export async function load(options: LoadDocumentsOptions): Promise<Sources> {
 	const { include = ["**/*"], exclude } = options
 
 	const sources = await loadDocuments(include, {
@@ -30,9 +40,32 @@ export async function load(options: LoadDocumentsOptions): Promise<Source[]> {
 		],
 	})
 
-	return sources
+	const hashed = await Promise.all(
+		sources
+			.filter((source): source is FullSource =>
+				Boolean(source.rawSDL && source.location),
+			)
+			.map(async function (source) {
+				return {
+					hash: await hash(source.rawSDL),
+					sdl: source.rawSDL,
+					location: source.location,
+				}
+			}),
+	)
+
+	const hashes = hashed.map((source) => source.hash).sort()
+
+	const result = {
+		hash: await hash(JSON.stringify(hashes)),
+		sources: hashed,
+	}
+
+	return result
 }
 
 export function watch(options: LoadDocumentsOptions) {
-	return watchFiles(options).pipe(concatMap(() => load(options)))
+	return watchFiles(options)
+		.pipe(concatMap(() => load(options)))
+		.pipe(distinct((x) => x.hash))
 }
