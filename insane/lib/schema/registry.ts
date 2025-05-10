160d43975b70a324e00dfc5c49df13d53b8cf576
Romeo Van Snick <romeo@romeovansnick.be>
2025-05-10 20:52:12 +0200

    Add builtin types

--- insane/lib/schema/registry.ts
+++ insane/lib/schema/registry.ts
@@ -2,4 +2,8 @@ export interface InsaneSchemaTypes {
 	__internal: "insane_schema_types"
 }
 
-export type InsaneDataType = Exclude<keyof InsaneSchemaTypes, "__internal">
+type BuiltinType = "string" | "integer" | "float" | "boolean"
+
+export type InsaneDataType =
+	| Exclude<keyof InsaneSchemaTypes, "__internal">
+	| BuiltinType
